bd269d1553dbf6f25ba9ff8d5e6ebf07779bd9d2
M Moersalin <ayinmursalin12@gmail.com>
2024-01-07 15:25:04 +0700

    Ping without Library

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
-#include <NewPing.h>
+// #include <NewPing.h>
 
 #define TRIGGER_PIN 2
 #define ECHO_PIN 3
@@ -8,19 +8,29 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-int getVolume();
-
-NewPing sonic = NewPing(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE_IN_CM);
+// NewPing sonic = NewPing(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE_IN_CM);
 
 void setup()
 {
   lcd.init();
   lcd.backlight();
+
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
 }
 
 void loop()
 {
-  unsigned long distance = sonic.ping_cm();
+  // unsigned long distance = sonic.ping_cm();
+
+  digitalWrite(TRIGGER_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+
+  unsigned long duration = pulseIn(ECHO_PIN, HIGH);
+  unsigned long distance = duration * 0.034 / 2;
 
   lcd.clear();
   lcd.setCursor(0, 0);
